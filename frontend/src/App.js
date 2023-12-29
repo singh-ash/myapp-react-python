b52fa8809c3a2b745487172c744ae4ece22a4f60
singh-ash <ashwini.aks@gmail.com>
2023-12-28 20:45:29 -0800

    fixed code with linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,26 +5,27 @@ import Search from './component/Search';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
-
 const App = () => {
   const [word, setWord] = useState('');
   const handleSerachSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      })
-    setWord('')
+      });
+    setWord('');
   };
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit={ handleSerachSubmit } />
+      <Search word={word} setWord={setWord} handleSubmit={handleSerachSubmit} />
     </div>
   );
 };
