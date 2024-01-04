1f6b03ee7d00a040a79182e41aed1a9625010bf6
singh-ash <ashwini.aks@gmail.com>
2024-01-03 16:31:33 -0800

    enabled dubug mode in flask

--- api/main.py
+++ api/main.py
@@ -8,12 +8,14 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL = os.environ.get("UNSPLASH_URL", "")
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError(
         "Please create .env.local file and inster there UNSPLASH_KEY"
     )
 app = Flask(__name__)
+app.config["DEBUG"] = DEBUG
 
 
 @app.route("/new-image")
